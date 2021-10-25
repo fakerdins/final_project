7d46ca03add7afcd2350f92fa233c5b3fc07fdb5
Sanjar Fakerdinov <yourgitemail@example.com>
2021-10-26 02:03:08 +0600

    account app created

--- account/models.py
+++ account/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.crypto import get_random_string
+
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email field is empty...')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.create_activation_code()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email field is empty...')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.create_activation_code()
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=254, unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=35, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} / {self.id}'
+
+    def create_activation_code(self):
+        code = get_random_string(length=10)
+        self.activation_code = code
---
+++ account/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .utils import send_activation_code
+from .models import CustomUser
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        min_length=4,required=True,
+        write_only=True
+    )
+    password_confirmation = serializers.CharField(
+        min_length=4,required=True,
+        write_only=True
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email', 'password',
+            'password_confirmation',           
+        )
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirmation = attrs.pop('password_confirmation')
+        if password != password_confirmation:
+            msg_ = ("Passwords are not matching...")
+            raise serializers.ValidationError(msg_)
+        return attrs
+
+    def create(sefl, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        send_activation_code(
+            user.email,
+            user.activation_code
+        )
+        return user
